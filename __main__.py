c60ae37b3a776b1413f77ceabfd55a85a9c9648a
mbrooke14 <bromat14@ecolecatholique.ca>
2025-01-04 22:02:24 -0500

    better debugging

--- __main__.py
+++ __main__.py
@@ -1,6 +1,10 @@
 from packet import ETH_Frame, ARP_Body, SEND_PACKET
 from addresses import MAC, IPv4
 
+from time import time
+
+t_0 = time()
+
 CONFIG = {
 
 	"INT": "wlan0",						# Network interface
@@ -16,6 +20,10 @@ CONFIG = {
 
 }
 
+# Show Config
+conf_str = "\n │  ".join([f"{k}: {str(v)}" for k, v in CONFIG.items()])
+print(f"\n\033[0;34m[{round(time() - t_0, 4)}s] Configuration:\n │  {conf_str}\033[0m\n")
+
 # Create Packet Parts
 # (ethernet frame + arp body)
 
@@ -26,16 +34,18 @@ arp_packet = ARP_Body(CONFIG["OP"],  CONFIG["SHA"], CONFIG["SPA"], CONFIG["THA"]
 
 packet = eth_frame.frm + arp_packet.pkt
 
-# Debugging
+# Show Generated Packet
 
-conf_str = "\n │  ".join([f"{k}: {str(v)}" for k, v in CONFIG.items()])
-pack_str = f"\n │  ethernet frame: {eth_frame.frm}\n │  packet body:    {arp_packet.pkt}\n │  full packet:    {packet}"
+pack_str = f"""
+ │  ethernet frame: {str(eth_frame.frm)[2:-1]}
+ │  packet body:    {str(arp_packet.pkt)[2:-1]}
+ │
+ │  full packet:    {str(packet)[2:-1]}"""
 
-print(f"\n\033[0;34mConfiguration:\n │  {conf_str}\033[0m\n")
-print(f"\033[0;32mPrepared ARP packet (pkt 1):\033[0;2m{pack_str}\033[0m\n")
+print(f"\033[0;32m[{round(time() - t_0, 4)}s] Prepared ARP packet (pkt 1):\033[0;2m{pack_str}\033[0m\n")
 
 # Send the packet
 
 SEND_PACKET(CONFIG["INT"], packet)
 
-print("\033[0;32mPacket Sent\033[0m\n")
+print(f"\033[0;32m[{round(time() - t_0, 4)}s] Packet Sent \033[0m\n")
